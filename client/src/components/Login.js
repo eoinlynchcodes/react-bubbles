fe0ae499af56298de2b4796c2f725afac16e9648
eoinlynchcodes <eoin95lynch@gmail.com>
2020-02-28 08:52:43 +0000

    Login works correctly.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,15 +1,38 @@
 import React, { useState } from "react";
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-
+  
+  const history = useHistory();
+  const baseURL = 'http://localhost:5000';
   const [ loginform, setLoginform ] = useState({
     username: '',
     password: '',
   })
 
-  const onChange = () => {
+  const onChange = (event) => {
+    setLoginform({
+      ...loginform,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post(`${baseURL}/api/login`, loginform)
+    .then((response) => {
+      console.log(response.data.payload);
+      // Need to pass the correct data to localStorage.
+      // Use console.log to check and ensure.
+      localStorage.setItem('token', response.data.payload)
+      history.push('/youmadeit');
+    })
+    .catch((error) => {
+      console.log(error);
+    })
 
   }
 
@@ -18,7 +41,7 @@ const Login = () => {
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
 
-      <form>
+      <form onSubmit={event => handleSubmit(event)}>
         <label>Username:</label>
         <input 
         name="username"

009b181d32e09bd2c86aeddea3ec3c7ae42f5e6e
eoinlynchcodes <eoin95lynch@gmail.com>
2020-02-28 09:07:40 +0000

    colors are returning and being displayed.

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -11,10 +11,12 @@ export const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+
   useEffect(() => {
     withAuth().get('http://localhost:5000/api/colors')
     .then((response) => {
-      console.log(response);
+      console.log(response.data);
+      setColorList(response.data);
     })
     .catch((error) => {
       alert('There has been an issue displaying the colour data. Please bear with us');
